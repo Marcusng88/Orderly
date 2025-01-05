6da8e7ea13f57b5edee6fdd5becb2b698942cd16
Jayden-Yong <jaydenyong28@gmail.com>
2025-01-06 00:57:26 +0800

    error

--- ToDoList.sql
+++ ToDoList.sql
@@ -1,49 +1,86 @@
-USE ToDoList;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: todolist
+-- ------------------------------------------------------
+-- Server version	8.0.40
 
-CREATE TABLE tasks (
-id INT AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(255) NOT NULL, 
-description TEXT, 
-status ENUM ('Incomplete', 'Complete') DEFAULT 'Incomplete',
-due_date DATE,
-category VARCHAR(50),
-priority ENUM ('Low', 'Moderate', 'High'),
-recurrence_interval ENUM ('Daily', 'Weekly', 'Monthly') DEFAULT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE task_dependencies (
-id INT AUTO_INCREMENT PRIMARY KEY, 
-task_id INT NOT NULL ,
-dependency_id INT NOT NULL, 
-FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE, 
-FOREIGN KEY (dependency_id) REFERENCES tasks(id) ON DELETE CASCADE,
-UNIQUE (task_id, dependency_id)
-);
+--
+-- Table structure for table `task_dependencies`
+--
 
-INSERT INTO tasks (title, description, due_date, category, priority)
-VALUES ('Complete research', 'Research for project', '2024-10-12', 'Homework', 'High');
+DROP TABLE IF EXISTS `task_dependencies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task_dependencies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `task_id` int NOT NULL,
+  `dependency_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `task_id` (`task_id`,`dependency_id`),
+  KEY `dependency_id` (`dependency_id`),
+  CONSTRAINT `task_dependencies_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `task_dependencies_ibfk_2` FOREIGN KEY (`dependency_id`) REFERENCES `tasks` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SELECT * FROM todolist.tasks;
+--
+-- Dumping data for table `task_dependencies`
+--
 
-INSERT INTO tasks (title, description, due_date, category, priority)
-VALUES ('FOM', 'Comic Project', '2025-01-06', 'Group Project', 'High');
+LOCK TABLES `task_dependencies` WRITE;
+/*!40000 ALTER TABLE `task_dependencies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task_dependencies` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO tasks (title, description, due_date, category, priority)
-VALUES ('CSO', 'Group Assignment', '2025-01-07', 'Group Project', 'High');
+--
+-- Table structure for table `tasks`
+--
 
-INSERT INTO tasks (title, description, due_date, category, priority)
-VALUES ('ENGLISH', 'Test 2', '2025-01-02', 'Individual', 'High');
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `status` enum('Incomplete','Complete') DEFAULT 'Incomplete',
+  `due_date` date DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `priority` enum('Low','Moderate','High') DEFAULT NULL,
+  `recurrence_interval` enum('Daily','Weekly','Monthly') DEFAULT 'Daily',
+  `next_occurrence` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SELECT * FROM todolist.tasks;
+--
+-- Dumping data for table `tasks`
+--
 
-INSERT INTO tasks (title, description, due_date, category, priority)
-VALUES ('FOP', 'to-do-list', '2025-01-03', 'Group Project', 'High');
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (1,'Complete research','Research for project','Incomplete','2024-10-13','Homework','High',NULL,NULL),(2,'FOM','Comic Project','Incomplete','2025-01-06','Group Project','High',NULL,NULL),(3,'FOP','DB','Incomplete','2025-04-02',NULL,NULL,'Monthly','2025-01-02');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-SELECT * FROM todolist.tasks;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-ALTER TABLE tasks ADD COLUMN dependencies TEXT DEFAULT NULL;
-
-SELECT * FROM todolist.tasks;
-
-DELETE FROM tasks WHERE id='6';
-
+-- Dump completed on 2025-01-04 11:32:16
